da64b024507a71b1f9baff5cb8a768f8c5a2002b
Felipe Polon <f.s.polon@gmail.com>
2023-12-16 03:41:48 -0300

    Get Endpoint First, create an function called getElementByTitle that will search a list and return the first element correspond to the title typed. Then, create a get endpoint in order to retrieve a get request made by the client when query a title or get all the data if nothing was queried.

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const app = express();
+const getElementByTitle = (title, elementList) => {
+  return elementList.find((element) => {
+    return element.title === title;
+  });
+};
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
@@ -7,6 +13,15 @@ app.use(express.static('public'));
 app.get('/budget', (req, res, next) => {
     res.send('Hello World')
 })
+app.get('/budget/envelope', (req, res, next) => {
+  if(req.query.title) {
+    const foundTitle = getElementByTitle(req.query.title, envelope);
+    res.send(foundTitle);
+  } else {
+  res.send(envelope)
+  }
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
